7e36ea358dc23c90645051cae837e2fbfa3084ce
nullchinchilla <nullchinchilla@pm.me>
2022-07-12 15:11:08 -0500

    add pretty

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,39 @@ mod envfile;
 mod runner;
 pub use envfile::*;
 pub use runner::*;
+use themelio_stf::melvm::Value;
+
+/// Converts a melvm value to a Melodeon-esque string representation.
+pub fn mvm_pretty(val: &Value) -> String {
+    match val {
+        Value::Int(i) => i.to_string(),
+        Value::Bytes(v) => {
+            let raw = (0..v.len()).map(|i| *v.get(i).unwrap()).collect::<Vec<_>>();
+            if let Some(string) = String::from_utf8(raw.clone()).ok().and_then(|s| {
+                if s.chars().all(|c| !c.is_control()) {
+                    Some(s)
+                } else {
+                    None
+                }
+            }) {
+                let quoted = snailquote::escape(&string);
+                if quoted.starts_with('\'') {
+                    quoted.replace('\'', "\"")
+                } else if quoted.starts_with('\"') {
+                    quoted.into_owned()
+                } else {
+                    format!("\"{}\"", quoted)
+                }
+            } else {
+                let raw_repr = hex::encode(raw);
+                format!("x\"{}\"", raw_repr)
+            }
+        }
+        Value::Vector(vv) => {
+            let vv: Vec<_> = (0..vv.len())
+                .map(|i| mvm_pretty(vv.get(i).unwrap()))
+                .collect();
+            format!("[{}]", vv.join(", "))
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -95,7 +95,7 @@ fn print_val_and_type(val: &Value, t: &Type, truthiness: bool) {
     } else {
         eprintln!("{}", format!("- : {:?}", t).as_str().bright_purple().bold());
     }
-    eprint!("{}", mvm_pretty(val));
+    eprint!("{}", melorun::mvm_pretty(val));
     if truthiness {
         if val.clone().into_bool() {
             eprintln!("{}", " (truthy)".bright_green());
@@ -104,40 +104,5 @@ fn print_val_and_type(val: &Value, t: &Type, truthiness: bool) {
         }
     } else {
         eprintln!()
-    }
-}
-
-// Converts a melvm value to a Melodeon-esque string representation.
-fn mvm_pretty(val: &Value) -> String {
-    match val {
-        Value::Int(i) => i.to_string(),
-        Value::Bytes(v) => {
-            let raw = (0..v.len()).map(|i| *v.get(i).unwrap()).collect::<Vec<_>>();
-            if let Some(string) = String::from_utf8(raw.clone()).ok().and_then(|s| {
-                if s.chars().all(|c| !c.is_control()) {
-                    Some(s)
-                } else {
-                    None
-                }
-            }) {
-                let quoted = snailquote::escape(&string);
-                if quoted.starts_with('\'') {
-                    quoted.replace('\'', "\"")
-                } else if quoted.starts_with('\"') {
-                    quoted.into_owned()
-                } else {
-                    format!("\"{}\"", quoted)
-                }
-            } else {
-                let raw_repr = hex::encode(raw);
-                format!("x\"{}\"", raw_repr)
-            }
-        }
-        Value::Vector(vv) => {
-            let vv: Vec<_> = (0..vv.len())
-                .map(|i| mvm_pretty(vv.get(i).unwrap()))
-                .collect();
-            format!("[{}]", vv.join(", "))
-        }
     }
 }
